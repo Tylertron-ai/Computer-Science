ca938e4148ca240a026cbb9d176695f2bb69f700
Tylertron-ai <820030@mystma.org>
2024-09-25 09:30:19 -0500

    Tyler is awesome V2

--- Assignments/Animalsurvey.py
+++ Assignments/Animalsurvey.py
@@ -1,11 +1,11 @@
-animal = input("What is your favorite animal\n>")
-why = input("Why are " + animal + "'s your favorite animal\n>")
-food = input("What food do " + animal + "'s eat\n>")
-home = input("What reigons do " + animal + "'s live in\n>")
-fact = input("What is a unique fact about " + animal + "'s\n>")
-habits = input("What are the habits of " + animal + "\n>")
-defense = input("What kind of defenses do " + animal + "'s have against predators\n>")
-ants = input("If a " + animal + " were to fight 1,000,000 ants who would win \n>")
-ex_wife = input("does a " + animal + " have any sort of symiotic relationship with other animals \n>")
-moon = input("If a " + animal + " was on the moon would it survive \n>")
+animal = input("What is your favorite animal\n>")         #favorite animal
+why = input("Why are " + animal + "'s your favorite animal\n>")      #reason it's their favorite
+food = input("What food do " + animal + "'s eat\n>")         #food they eat
+home = input("What reigons do " + animal + "'s live in\n>")  #where they live
+fact = input("What is a unique fact about " + animal + "'s\n>") #interesting fact
+habits = input("What are the habits of " + animal + "\n>")     #things they like to do
+defense = input("What kind of defenses do " + animal + "'s have against predators\n>") #nobody's lunch
+ants = input("If a " + animal + " were to fight 1,000,000 ants who would win \n>")     #ants vs. other animals
+ex_wife = input("does a " + animal + " have any sort of symiotic relationship with other animals \n>") #BFFS
+moon = input("If a " + animal + " was on the moon would it survive \n>") #first animal on the moon
 print("your favorite animal is a " + animal + " it's your favorite animal because " + why + " your favorite animal likes to eat " + food + " your favorite animal lives in the " + home + " a fun fact about your favorite animal is " + fact + " the habits of your favorite animal are " + habits + " If my favorite animal were in trouble it would " + defense + " could my favorite animal beat 1,000,000 ants " + ants + " what animals does my favoite animal have a symboitic relationship with" + ex_wife + " could my favorite animal survive on the moon " + moon)
