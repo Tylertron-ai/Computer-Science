7d7b449aeb62ac3a5ad6570ce85cf60f8fad6864
Tylertron-ai <820030@mystma.org>
2024-09-27 09:09:57 -0500

    madlib

--- Assignments/functions.py
+++ Assignments/functions.py
@@ -1,2 +1,14 @@
-def greet():
-    print
+def top_five_movies(movie_1, movie_2, movie_3, movie_4, movie_5):
+    print("Tyler's top five movies")
+    print("1. " + movie_1)
+    print("2. " + movie_2)
+    print("3. " + movie_3)
+    print("4. " + movie_4)
+    print("5. " + movie_5)
+
+def add (x,y):
+    print(x + y)
+    
+num_1 = input("whats the first number?\n>")
+num_2 = input("what's the second number?\n>")
+add(num_1, num_2)
